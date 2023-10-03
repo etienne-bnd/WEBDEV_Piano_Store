2a1c62bee8f87c94b2be3fae4e0ced4c590715e1
etienne-bnd <146837769+etienne-bnd@users.noreply.github.com>
2023-10-03 19:58:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-function renderPianos(data) {
-  const container = document.getElementById('pianos-container');
-  
-  data.forEach(piano => {
-    const pianoCard = document.createElement('div');
-    pianoCard.classList.add('piano-card');
-    pianoCard.innerHTML = `
-      <h2>${piano[0]}</h2>
-      <p>${piano[1]}</p>
-      <p>Price: $${piano[2]}</p>
-      <p>Condition: ${piano[3]}</p>
-      <p>Location: ${piano[4]}</p>
-      <img src="${piano[5]}" alt="${piano[0]}">
-    `;
-    container.appendChild(pianoCard);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+    fetchPianoData();
+});
+
+function fetchPianoData() {
+    fetch('https://spreadsheets.google.com/feeds/list/1urRoHKR75u1kbWdPuAb4RlI7DLJQr8T5zNw8LB9OY10/od6/public/values?alt=json') 
+        .then(response => response.json())
+        .then(data => {
+            const pianos = data.feed.entry;
+            renderPianoList(pianos);
+        });
 }
 
-// Fetch data from Google Sheet
-fetch('https://script.google.com/macros/s/AKfycbyEXAMPLE/exec')
-  .then(response => response.json())
-  .then(data => renderPianos(data));
+function renderPianoList(pianos) {
+    const pianoListElement = document.getElementById('piano-list');
+
+    pianos.forEach(piano => {
+        const name = piano.gsx$name.$t;
+        const type = piano.gsx$type.$t;
+        const price = piano.gsx$price.$t;
+
+        const pianoCard = document.createElement('div');
+        pianoCard.classList.add('piano-card');
+        pianoCard.innerHTML = `
+            <h2>${name}</h2>
+            <p>Type: ${type}</p>
+            <p>Price: $${price}</p>
+        `;
+        pianoListElement.appendChild(pianoCard);
+    });
+}
