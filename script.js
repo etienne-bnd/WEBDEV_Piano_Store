5a8e62caff4e29a3a4913d599c6a1d3932a5496e
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-05-02 17:50:55 +0200

    mise en place du vrai lien de la database de papa pour tout mettre en ligne d'un coup

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function fetchData() {
     // Récupérer les données JSON
-    console.log("v2");
-    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=SQa2rPdkYS217sCty3uz6eNVaZA0l295bQblnJe_8zKA-rtNdem-DE0-GDtnYIisM_cvrE6gLhAgJiVXEDJdcRcvGp4blP_mm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMj7zaZZCyyJg1PUAZFZnkYmk0dlf1ec8LoU-eNiELlVfoQMsthPU4b9rStfxXJ-An3Jiw3FCEOBl7HuJQ3hIpZLQEB_JWtpatz9Jw9Md8uu&lib=ML1OtzSUDqqBv5Rhp8Ibevq8tNrNXU7ET");
+    console.log("v3");
+    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=w5ASxkUfjgjOcm0w6dLJJZkONjDIlpSZhW1PxWBKhBct_qIkmXTcL2tTZ-uk0oJ3XpAHQMr5GCC5EMI_CgKW-8C3tJY1Y6eWm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIc-e-cSbcZET-UBv6SiWoXy4aU7StCWJngkkZyfGGeBPye3SZwiXHDzHnA-3GMJ-_v6tYKOXfb5tw70HZzkmcsv1HTz7-lN99z9Jw9Md8uu&lib=Ml1e-lb1qAMfOSe-7BliAWq8tNrNXU7ET");
     const json = await res.json();
     return json.data; // Accéder à la propriété 'data'
   }
