c2ea8eabbb526ea93e4410ebdbefa6e6149e84dc
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-05-02 19:01:56 +0200

    petite mise en forme et ajout de la marque du piano

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function fetchData() {
     // Récupérer les données JSON
     console.log("v3");
-    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=w5ASxkUfjgjOcm0w6dLJJZkONjDIlpSZhW1PxWBKhBct_qIkmXTcL2tTZ-uk0oJ3XpAHQMr5GCC5EMI_CgKW-8C3tJY1Y6eWm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIc-e-cSbcZET-UBv6SiWoXy4aU7StCWJngkkZyfGGeBPye3SZwiXHDzHnA-3GMJ-_v6tYKOXfb5tw70HZzkmcsv1HTz7-lN99z9Jw9Md8uu&lib=Ml1e-lb1qAMfOSe-7BliAWq8tNrNXU7ET");
+    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=h73LG2STHokgsLY5v737iaL0soLPcktBwoXMYpimpYmGJ5J_mNfO-V2c2x8emAotBqs3SXuTbzmW7ppcBZ0o-uq-L250hG9hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDw4Aowd6dEcBKwet8QX93jDfxvjxsroDzrRUkFUa29krRR_fcV6d3usC_DavSbpATDl147DuKf-9Ww8gQXIz1uN0E_1dFcejNz9Jw9Md8uu&lib=Ml1e-lb1qAMfOSe-7BliAWq8tNrNXU7ET");
     const json = await res.json();
     return json.data; // Accéder à la propriété 'data'
   }
@@ -17,12 +17,17 @@ async function fetchData() {
             
   
       pianoElement.innerHTML = `
-        <h2>${piano.numero}</h2>
+        <h4>${piano.numero}</h4>
+        <h3>piano ${piano.marque}</h3>
         <p>Couleur: ${piano.couleur}</p>
-        <p>Prix: ${piano.prix}</p>
+        <p>Piano à vendre: ${piano.prix}</p>
         <p>Prix de location: ${piano.prixloc}</p>
-        <p>Photos: <a href="${piano.photos}">${piano.photos}</a></p>
-        <img src="${piano.photo_site}" alt="Photo du piano"  style="width: 300px; height: 200px;"/>
+
+        <div style="text-align: center;">
+          <img src="${piano.photo_site}" alt="Photo du piano à louer" style="max-width: 100%; max-height: 300px;">
+        </div>
+        <p><a href="${piano.photos}">voir plus de photos</a></p>
+
       `;
   
       container.appendChild(pianoElement);
