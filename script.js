1b030245558b95030178d2757d33a366dcb42fe1
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-05-06 20:01:54 +0200

    ajout d'un tri qui affiche que les pianos qui sont disponible plus style du bouton pour les liens et disparation des logos images moche quand il y en a pas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function fetchData() {
     // Récupérer les données JSON
     console.log("v4");
-    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=h73LG2STHokgsLY5v737iaL0soLPcktBwoXMYpimpYmGJ5J_mNfO-V2c2x8emAotBqs3SXuTbzmW7ppcBZ0o-uq-L250hG9hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDw4Aowd6dEcBKwet8QX93jDfxvjxsroDzrRUkFUa29krRR_fcV6d3usC_DavSbpATDl147DuKf-9Ww8gQXIz1uN0E_1dFcejNz9Jw9Md8uu&lib=Ml1e-lb1qAMfOSe-7BliAWq8tNrNXU7ET");
+    const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=kbYKx7JtNWNE1Z8VmgtFgIYanV4xXeeV-SlXrKeoOdPqtjl_Pvnby5K-6JGa2ekhKOJRgOFHrtyrAQPeWSJog_zZ3Ed-8Yhkm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCLmOJ7fQgJOfBHICit4qpEmRHCp4mVnFfJi0LmdnuunsMhM-h10RfQiQGq1TLOo7biArXcshSGuLcP2bvRXVA8hpIpPm45QStz9Jw9Md8uu&lib=Ml1e-lb1qAMfOSe-7BliAWq8tNrNXU7ET");
     const json = await res.json();
     return json.data; // Accéder à la propriété 'data'
   }
@@ -12,26 +12,41 @@ async function fetchData() {
     var container = document.getElementById("pianos-container");
   
     pianosData.forEach(function(piano) {
-      // Vérifier si la ligne d'information est vide
-      if (piano.numero || piano.marque || piano.couleur || piano.prix || piano.prixloc || piano.photo_site || piano.photos) {
+      // Vérifier si la ligne d'information est vide  
+      if (!piano.dispo) {
         var pianoElement = document.createElement("div");
         pianoElement.classList.add("piano");
-  
+    
         pianoElement.innerHTML = `
           <h4>${piano.numero}</h4>
           <h3>piano ${piano.marque}</h3>
           <p>Couleur: ${piano.couleur}</p>
           <p>Piano à vendre: ${piano.prix}</p>
           <p>Prix de location: ${piano.prixloc}</p>
-  
-          <div style="text-align: center;">
-            <img src="${piano.photo_site}" alt="Photo du piano à louer" >
-          </div>
-          <p><a href="${piano.photos}">voir plus de photos</a></p>
-        `;
-  
+          `;
+          if (piano.photo_site) {
+            var imageContainer = document.createElement("div"); // Créer un conteneur pour l'image
+            imageContainer.classList.add("image-container");
+
+            var imageElement = document.createElement("img");
+            imageElement.src = piano.photo_site;
+            imageElement.alt = "Photo du piano à louer";
+            imageContainer.appendChild(imageElement); // Ajouter l'image dans le conteneur
+            pianoElement.appendChild(imageContainer); // Ajouter le conteneur dans l'élément piano
+        }
+
+        if (piano.photos) {
+            var linkElement = document.createElement("a");
+            linkElement.href = piano.photos;
+            linkElement.textContent = "voir plus de photos";
+            linkElement.classList.add("lien_photo"); // Ajout de la classe "lien_photo"
+            var paragraphElement = document.createElement("p");
+            paragraphElement.appendChild(linkElement);
+            pianoElement.appendChild(paragraphElement);
+        }
+
         container.appendChild(pianoElement);
-      }
+    }
     });
   }
   
