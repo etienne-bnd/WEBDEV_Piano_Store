d60c7dce0569db7f8d5ab75eab9c61662c6abd04
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-09-30 10:49:24 +0200

    réglage du problème d'affichage du numéro sur téléphone

--- script.js
+++ script.js
@@ -16,98 +16,98 @@ async function fetchData() {
 
 var pianofirst = fetchData();
 // plus forcément utile maintenant qu'il y en a une avec des conditions
-async function afficherPianos() {
-  const pianosData = await fetchData();
-  var container = document.getElementById("pianos-container");
-  // on récupère l'élément piano-container du code html
-  container.innerHTML = ``;// Vider tout le contenu HTML de la division
+// async function afficherPianos() {
+//   const pianosData = await fetchData();
+//   var container = document.getElementById("pianos-container");
+//   // on récupère l'élément piano-container du code html
+//   container.innerHTML = ``;// Vider tout le contenu HTML de la division
 
-  // on créer pour chaque case ce qu'il faut 
-  pianosData.forEach(function(piano) {
-    // Vérifier si la ligne d'information est vide  
-    if ((!piano.dispo) && (piano.numero || piano.marque || piano.couleur|| piano.prix || piano.prixloc || piano.photo_site || piano.photos || piano.descriptif)) {
-      // si le piano est dispo et qu'au moins une case est remplie
-      var pianoElement = document.createElement("div");
-      // on créer une nouvelle division
-      pianoElement.classList.add("piano");
-        // on lui ajoute la classe piano
-        pianoElement.innerHTML = ``;
-        if (piano.numero) {
-        pianoElement.innerHTML += `
-          <h4 id='piano_num'>${piano.numero}</h4>
-                    `;
-        }
-        if (piano.marque) {
-        pianoElement.innerHTML += `
-          <h3>Piano ${piano.marque}</h3>
-                              `;
-        }
-        if (piano.photo_site) {
-          var imageContainer = document.createElement("div"); // Créer un conteneur pour l'image
-          imageContainer.classList.add("image-container");
+//   // on créer pour chaque case ce qu'il faut 
+//   pianosData.forEach(function(piano) {
+//     // Vérifier si la ligne d'information est vide  
+//     if ((!piano.dispo) && (piano.numero || piano.marque || piano.couleur|| piano.prix || piano.prixloc || piano.photo_site || piano.photos || piano.descriptif)) {
+//       // si le piano est dispo et qu'au moins une case est remplie
+//       var corpscase = document.createElement("div");
+//       // on créer une nouvelle division
+//       corpscase.classList.add("piano");
+//         // on lui ajoute la classe piano
+//         corpscase.innerHTML = ``;
+//         if (piano.numero) {
+//         corpscase.innerHTML += `
+//           <h4 id='piano_num'>${piano.numero}</h4>
+//                     `;
+//         }
+//         if (piano.marque) {
+//         corpscase.innerHTML += `
+//           <h3>Piano ${piano.marque}</h3>
+//                               `;
+//         }
+//         if (piano.photo_site) {
+//           var imageContainer = document.createElement("div"); // Créer un conteneur pour l'image
+//           imageContainer.classList.add("image-container");
 
 
-          var imageElement = document.createElement("img");
-          imageElement.src = piano.photo_site;
-          imageElement.alt = "Photo du piano à louer";
-          imageContainer.appendChild(imageElement); // Ajouter l'image dans le conteneur
-          pianoElement.appendChild(imageContainer); // Ajouter le conteneur dans l'élément piano
-        }
+//           var imageElement = document.createElement("img");
+//           imageElement.src = piano.photo_site;
+//           imageElement.alt = "Photo du piano à louer";
+//           imageContainer.appendChild(imageElement); // Ajouter l'image dans le conteneur
+//           corpscase.appendChild(imageContainer); // Ajouter le conteneur dans l'élément piano
+//         }
 
-        if (piano.prix) {
-        pianoElement.innerHTML += `
-          <p>Piano à vendre: <span class='bold-text'>${piano.prix}</span></p>
-          `;
-        }
-        if (piano.prixloc) {
-                pianoElement.innerHTML += `
-          <p>Prix de location: <span class='bold-text'> ${piano.prixloc}</span></p>
-          `;
-        }
+//         if (piano.prix) {
+//         corpscase.innerHTML += `
+//           <p>Piano à vendre: <span class='bold-text'>${piano.prix}</span></p>
+//           `;
+//         }
+//         if (piano.prixloc) {
+//                 corpscase.innerHTML += `
+//           <p>Prix de location: <span class='bold-text'> ${piano.prixloc}</span></p>
+//           `;
+//         }
         
-        if (piano.couleur) {
-          pianoElement.innerHTML += `
-          <p>Couleur: <span class='bold-text'>${piano.couleur}</span></p>
-          `;
-        }
-        if (piano.profondeur && piano.largeur)
-          {
-            if (piano.hauteur) {
-              pianoElement.innerHTML += `          
-              <p>Dimensions: <span class='bold-text'>${piano.hauteur}</span>x<span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
-            `;
-            }
-            else {
-              pianoElement.innerHTML += ` 
-              <p>Dimensions: <span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
-            `;
-            }
-          }
-        if (piano.photos) {
-          var linkElement = document.createElement("a");
-          linkElement.href = piano.photos;
-          linkElement.textContent = "plus de photos du piano";
-          linkElement.textContent = "plus de photos du piano";
-          linkElement.classList.add("lien_photo"); // Ajout de la classe "lien_photo"
-          var paragraphElement = document.createElement("p");
-          paragraphElement.appendChild(linkElement);
-          pianoElement.appendChild(paragraphElement);
-        }
+//         if (piano.couleur) {
+//           corpscase.innerHTML += `
+//           <p>Couleur: <span class='bold-text'>${piano.couleur}</span></p>
+//           `;
+//         }
+//         if (piano.profondeur && piano.largeur)
+//           {
+//             if (piano.hauteur) {
+//               corpscase.innerHTML += `          
+//               <p>Dimensions: <span class='bold-text'>${piano.hauteur}</span>x<span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
+//             `;
+//             }
+//             else {
+//               corpscase.innerHTML += ` 
+//               <p>Dimensions: <span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
+//             `;
+//             }
+//           }
+//         if (piano.photos) {
+//           var linkElement = document.createElement("a");
+//           linkElement.href = piano.photos;
+//           linkElement.textContent = "plus de photos du piano";
+//           linkElement.textContent = "plus de photos du piano";
+//           linkElement.classList.add("lien_photo"); // Ajout de la classe "lien_photo"
+//           var paragraphElement = document.createElement("p");
+//           paragraphElement.appendChild(linkElement);
+//           corpscase.appendChild(paragraphElement);
+//         }
       
-        if (piano.descriptif) {
+//         if (piano.descriptif) {
 
-          pianoElement.innerHTML+= `
-          <p>${piano.descriptif}</p>
-          `;
-        }
-    container.appendChild(pianoElement);
-    }
-  });
-  var gifElement = document.getElementById('chargement');
-  gifElement.classList.add('hidden'); 
-  // Ajoute la classe .hidden pour masquer en douceur
-  gifElement.style.display = 'none'; // Masque l'élément GIF après un délai de 2 secondes
-}
+//           corpscase.innerHTML+= `
+//           <p>${piano.descriptif}</p>
+//           `;
+//         }
+//     container.appendChild(corpscase);
+//     }
+//   });
+//   var gifElement = document.getElementById('chargement');
+//   gifElement.classList.add('hidden'); 
+//   // Ajoute la classe .hidden pour masquer en douceur
+//   gifElement.style.display = 'none'; // Masque l'élément GIF après un délai de 2 secondes
+// }
 
 async function afficherPianosConditions(couleurselectionne="TOUT", marqueselectionne="TOUT") {
   let pianosData = await pianofirst;
@@ -120,17 +120,22 @@ async function afficherPianosConditions(couleurselectionne="TOUT", marqueselecti
     if ((marqueselectionne === "TOUT" || piano.marque === marqueselectionne) && (couleurselectionne === "TOUT" || piano.couleur === couleurselectionne) && (!piano.dispo) && (piano.numero || piano.marque || piano.couleur|| piano.prix || piano.prixloc || piano.photo_site || piano.photos || piano.descriptif)) {
       // si le piano est dispo et qu'au moins une case est remplie
       var pianoElement = document.createElement("div");
+      var corpscase = document.createElement("div");
+      pianoElement.classList.add("piano")
+      pianoElement.appendChild(corpscase);
+      // peut être ne sert à rien parce que supprimé juste après
       // on créer une nouvelle division
-      pianoElement.classList.add("piano");
+      corpscase.classList.add("corpscasepiano");
         // on lui ajoute la classe piano
-        pianoElement.innerHTML = ``;
+        corpscase.innerHTML = ``;
         if (piano.numero) {
-        pianoElement.innerHTML = `
+        pianoElement.innerHTML += `
           <h4 id='piano_num'>${piano.numero}</h4>
                     `;
         }
+        pianoElement.appendChild(corpscase);
         if (piano.marque) {
-        pianoElement.innerHTML += `
+        corpscase.innerHTML += `
           <h3>Piano ${piano.marque}</h3>
                               `;
         }
@@ -143,34 +148,34 @@ async function afficherPianosConditions(couleurselectionne="TOUT", marqueselecti
           imageElement.src = piano.photo_site;
           imageElement.alt = "Photo du piano à louer";
           imageContainer.appendChild(imageElement); // Ajouter l'image dans le conteneur
-          pianoElement.appendChild(imageContainer); // Ajouter le conteneur dans l'élément piano
+          corpscase.appendChild(imageContainer); // Ajouter le conteneur dans l'élément piano
         }
 
         if (piano.prix) {
-        pianoElement.innerHTML += `
+        corpscase.innerHTML += `
           <p>Piano à vendre: <span class='bold-text'>${piano.prix}</span></p>
           `;
         }
         if (piano.prixloc) {
-                pianoElement.innerHTML += `
+                corpscase.innerHTML += `
           <p>Prix de location du piano: <span class='bold-text'> ${piano.prixloc}</span></p>
           `;
         }
         
         if (piano.couleur) {
-          pianoElement.innerHTML += `
+          corpscase.innerHTML += `
           <p>Couleur: <span class='bold-text'>${piano.couleur}</span></p>
           `;
         }
         if (piano.profondeur && piano.largeur)
           {
             if (piano.hauteur) {
-              pianoElement.innerHTML += `          
+              corpscase.innerHTML += `          
               <p>Dimensions: <span class='bold-text'>${piano.hauteur}</span>x<span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
             `;
             }
             else {
-              pianoElement.innerHTML += ` 
+              corpscase.innerHTML += ` 
               <p>Dimensions: <span class='bold-text'>${piano.profondeur}</span>x<span class='bold-text'>${piano.largeur} cm </span></p>
             `;
             }
@@ -182,12 +187,12 @@ async function afficherPianosConditions(couleurselectionne="TOUT", marqueselecti
           linkElement.classList.add("lien_photo"); // Ajout de la classe "lien_photo"
           var paragraphElement = document.createElement("p");
           paragraphElement.appendChild(linkElement);
-          pianoElement.appendChild(paragraphElement);
+          corpscase.appendChild(paragraphElement);
         }
       
         if (piano.descriptif) {
 
-          pianoElement.innerHTML+= `
+          corpscase.innerHTML+= `
           <p>${piano.descriptif}</p>
           `;
         }
