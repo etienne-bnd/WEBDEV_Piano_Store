6dd77b00d56269e290c352a1f3f1c22dc02023d5
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-09-30 23:51:42 +0200

    ajout du son sur les touches

--- demarrage.js
+++ demarrage.js
@@ -7,28 +7,28 @@ function write_header() {
         <div class="touchen"></div>
         <div class="toucheb"></div>
         <div class="touchen"></div>
-        <a href="accord.html">
+        <a href="accord.html" data-note="261.63">
         ACCORD DE PIANO
         </a>
         <div class="touchen"></div>
-        <a href="debarras.html">
+        <a href="debarras.html" data-note="293.66">
         DEBARRAS DE PIANO
         </a>
         <div class="touchen"></div>
-        <a href="piano-a-louer.html">
+        <a href="piano-a-louer.html" data-note="329.63">
         CATALOGUE<br>
         DES PIANOS</a>
         <div class="touchen milieu"></div>
-        <a href="achatlocation.html">
+        <a href="achatlocation.html" data-note="349.23">
         ACHAT
         VENTE
         LOCATION
         DE PIANO
         </a>
         <div class="touchen milieu"></div>
-        <a href="contact.html">CONTACT</a>
+        <a href="contact.html" data-note="392.00">CONTACT</a>
         <div class="touchen"></div>
-        <a href="a_propos.html">
+        <a href="a_propos.html" data-note="440.00">
         A PROPOS
         </a>
         <div class="touchen"></div>
@@ -130,7 +130,43 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     console.log("Ce n'est pas un téléphone !");
     // Exécuter le script pour les autres appareils ici
 }
+function playPianoNote(frequency) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.type = 'sine'; // Type d'onde sinusoïdale
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime); // Définir la fréquence
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    gainNode.gain.setValueAtTime(0.005, audioContext.currentTime); // Volume initial
+    gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 1.5); // Réduction progressive
+
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 1.5); // Arrêter après 1.5 secondes
+}
+
+// Sélectionner tous les liens ou titres dans le header
+const headerLinks = document.querySelectorAll('header a');
+
+// Ajouter un événement mouseover pour jouer la note lorsque la souris passe au-dessus de chaque lien
+headerLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        // Récupérer la fréquence de la note à partir de l'attribut data-note
+        // Jouer la note correspondante
+        const frequency = parseFloat(link.getAttribute('data-note'));
+
+        noteTimeout = setTimeout(() => {
+            playPianoNote(frequency);
+        }, 300); // 1000 ms = 1 seconde
+    });
+        link.addEventListener('mouseout', () => {
+        // Annuler le timeout si la souris sort du lien
+        clearTimeout(noteTimeout);
+    });
+});
 creation_bouton_marque();
 creation_bouton_couleur();
 console.log("la fonction a fonctionné");
