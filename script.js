97d9d80919e29f236d0093eeeb7c3b3929b6fa3c
etienne.bonnand@free.fr <etienne.bonnand@student-cs.fr>
2024-10-04 10:25:35 +0200

    mise à jour de l'ancienne version de l'app script

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function fetchData() {
 
 
     // Récupérer les données JSON
-    const res = await fetch("https://script.google.com/macros/s/AKfycbye1vWjELtjX_bSMe1gseGYPZuEzTBaymvwS26p-7D2D3p2sGiiUM9UioEBopQ6HCGXKA/exec");
+    const res = await fetch("https://script.google.com/macros/s/AKfycbzQn_XxuJsw8Z8m8P2soyRT-18hPKuJ15uurpfohI-i3mtZCQZ6-YYcPJ5sKhWpb-g-4A/exec");
     // le lien est à changer si on change le déployement de l'app script
     const json = await res.json();
     return json.data; // Accéder à la propriété 'data'
