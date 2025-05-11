0280de2a1a1cd2429c0ddf34f8aad135ea7ddd17
etienne-bnd <146837769+etienne-bnd@users.noreply.github.com>
2025-05-11 18:25:29 +0200

    oubli d'un crochet

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ async function fetchData() {
 
   try {
     // Récupérer les données JSON
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyqZLLCuWMmWbCJrr6HZnK0KkyOKjOzvPgF81nIyhiJwposswC8KsJYw7svx-66FwMW/exec");
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyqZLLCuWMmWbCJrr6HZnK0KkyOKjOzvPgF81nIyhiJwposswC8KsJYw7svx-66FwMW/exec");}
+  
     
     // Vérifier si la requête a réussi (status 200-299)
     if (!response.ok) {
