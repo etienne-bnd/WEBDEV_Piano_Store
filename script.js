e091ccbcbe7741596cfa1915cd9eaa14bc7c763c
etienne-bnd <146837769+etienne-bnd@users.noreply.github.com>
2025-11-09 22:15:03 +0100

    Change fetch source to local pianos.json
    Updated fetch URL to use local JSON file instead of remote script.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function fetchData() {
   pianoContainer.innerHTML = htmlContent;
 
     // Récupérer les données JSON
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyqZLLCuWMmWbCJrr6HZnK0KkyOKjOzvPgF81nIyhiJwposswC8KsJYw7svx-66FwMW/exec");
+    const response = await fetch("./pianos.json");
 
     const json = await response.json(); // Cette ligne doit être dans le bloc `try`
     return json.data;
